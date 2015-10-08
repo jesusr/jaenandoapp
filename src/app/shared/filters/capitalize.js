afe8e59668e5b813b3446399a4b8f3828ad917b2
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-10-08 12:30:38 +0200

    Filters module and Capitalize

---
+++ src/app/shared/filters/capitalize.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+  angular.module('app.filters')
+    .filter('capitalize', function() {
+      return function(input) {
+        if (input !== null) {
+          input = input.toLowerCase();
+        }
+        return input.substring(0, 1).toUpperCase() + input.substring(1);
+      };
+    });
+})();
+
