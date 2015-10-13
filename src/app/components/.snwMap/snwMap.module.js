6ef620afddd22ae9cb5a37dd667720c91082c0a5
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-10-13 12:01:32 +0200

    clean

--- src/app/components/.snwMap/snwMap.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-  angular.module('snwMap', ['snwMap.gmap', 'snwMap.zoomControl', 'snwMap.staticFilter'])
-    .config(function(GoogleMapApiProvider) {
-      GoogleMapApiProvider.configure({});
-    });
-})();
-
