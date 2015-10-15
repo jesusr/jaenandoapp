9cd7506ad15ec58bf12f047764d93dfe04e38f9e
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-10-15 23:33:47 +0200

    push

--- src/app/components/podcast/podcastCtrl.js
+++ src/app/components/podcast/podcastCtrl.js
@@ -5,6 +5,22 @@
     podcastServ.load(function(data) {
       $scope.data = data;
     });
+    $scope.setHighlight = function(p, n) {
+      var elem, elems, l, i, toggle;
+      elem = document.querySelector('.' + p.slug);
+      elems = document.querySelectorAll('.podcast');
+      toggle = elem.className.indexOf('highlight') > 0;
+      l = elems.length;
+      for (i = 0; i < l; i++) {
+        elems[i].className = elems[i].className.split(' ').filter(function(n) {
+          return n !== 'highlight';
+        }).join(' ');
+      }
+
+      if (n !== null && !toggle) {
+        elem.className += ' highlight';
+      }
+    };
     init();
   };
   angular.module('app.podcast').controller('podcastCtrl', podcastCtrl);
--- src/app/components/podcast/podcastDir.js
+++ src/app/components/podcast/podcastDir.js
@@ -4,6 +4,7 @@
     return {
       link: function() {},
       templateUrl: '/app/components/podcast/podcast.html',
+      controller: 'podcastCtrl',
       scope: {
         p: '=p'
       }
