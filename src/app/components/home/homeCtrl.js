31ac51da675e994b60cc721087227c6433d9ffc0
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-10-24 22:46:20 +0200

    inheritance from home ctrl

--- src/app/components/home/homeCtrl.js
+++ src/app/components/home/homeCtrl.js
@@ -2,7 +2,20 @@
   'use strict';
   var homeCtrl, config;
 
-  homeCtrl = function() {};
+  homeCtrl = function(podcastServ, $scope) {
+    podcastServ.load(function(data) {
+      var l, i;
+      $scope.podcastsById = {};
+      l = data.length;
+      for (i = 0; i < l; i++) {
+        if (typeof $scope.podcastsById[data[i].terms.category[0].slug] === 'undefined') {
+          $scope.podcastsById[data[i].terms.category[0].slug] = [];
+        }
+        $scope.podcastsById[data[i].terms.category[0].slug].push(data[i]);
+      }
+      $scope.podcasts = data;
+    });
+  };
 
   config = function($routeProvider) {
     $routeProvider
