299dd7c1b1f5476228dbd218ebddfd6cec735e04
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-10-24 22:45:14 +0200

    mapService

---
+++ src/app/shared/services/mapServ.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+  angular.module('app')
+    .service('mapServ', function($http) {
+      return {
+        load: function(callback) {
+          $http.get('/dist/data/data.json').success(function(data) {
+            window.console.log(data);
+            callback(data);
+          });
+        }
+      };
+    });
+})();
+
